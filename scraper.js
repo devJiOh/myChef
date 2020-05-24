5e05a9bc0f42e146558e4d0189ffaf3e5c137a66
Joseph Bulan <jbulan@optusnet.com.au>
2020-05-24 20:14:46 +1000

    Crawl all pages to scrape

--- scraper.js
+++ scraper.js
@@ -2,25 +2,45 @@ const cheerio = require('cheerio');
 const axios = require('axios');
 const rp = require('request-promise');
 
-const url = 'https://www.aldi.com.au/en/groceries/pantry/chocolate/';
+const crawl = async(crawlUrls) => {
+    await axios.get(crawlUrls)
+        .then(response => {
+            let getData = html => {
+                data = [];
+                const $ = cheerio.load(html);
 
-axios.get(url)
-    .then(response => {
-        let getData = html => {
-            data = [];
-            const $ = cheerio.load(html);
-
-
-            $('.box--description--header').each((i, elem) => {
-                data.push({
-                    title : $(elem).text()
+                $('.ratio-container.m-ratio-57x25.box--description.m-no-ratio-on-phone').each((i, elem) => {
+                    data.push({
+                        title : $(elem).find('.box--description--header').text().trim(),
+                        price : $(elem).find('.box--value,.box--decimal').text().trim(),
+                        ratio : $(elem).find('.box--baseprice').text().trim()
+                    });
                 });
-            });
 
-            console.log(data);
-        }
-        getData(response.data)
-    })
-    .catch(error => {
-        console.log('error');
-    })
+                console.log(data);
+            }
+            getData(response.data)
+        })
+        .catch(error => {
+            console.log('error');
+        })
+};
+
+const aldiUrl = 'https://www.aldi.com.au/en/groceries';
+const urls = [`${aldiUrl}/super-savers/`,
+    `${aldiUrl}/fresh-produce/dairy-eggs/`,
+    `${aldiUrl}/baby/nappies-and-wipes/`, `${aldiUrl}/baby/baby-food/`,
+    `${aldiUrl}/beauty/`, `${aldiUrl}/freezer/`,
+    `${aldiUrl}/health/`, `${aldiUrl}/laundry-household/laundry/`,
+    `${aldiUrl}/laundry-household/household/`, `${aldiUrl}/liquor/wine/`,
+    `${aldiUrl}/liquor/beer-cider/`, `${aldiUrl}/liquor/champagne-sparkling/`,
+    `${aldiUrl}/liquor/spirits/`, `${aldiUrl}/pantry/gluten-free/`,
+    `${aldiUrl}/pantry/chocolate/`, `${aldiUrl}/pantry/olive-oil/`,
+    `${aldiUrl}/pantry/just-organic/`, `${aldiUrl}/pantry/coffee/`
+    ];
+
+
+// not listed: fruit-veg, aus grown, meat
+urls.forEach(function(value){
+    crawl(value);
+})
